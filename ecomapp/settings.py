e05f3e0da2b6bc80c61c9e08dba7b997947b9787
Mark Nguyen <dinhdanh.nguyen@sjsu.edu>
2024-10-14 18:07:39 -0700

    fix kubernetes

--- ecomapp/settings.py
+++ ecomapp/settings.py
@@ -177,36 +177,37 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-# STATIC_URL = '/static/'
-# STATIC_ROOT = BASE_DIR /'static'
-# print("what is base directory: ", BASE_DIR)
-# STATICFILES_DIRS = [
-#     'ecomapp/static',
-# ]
-
-AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
-
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-# print(AWS_STORAGE_BUCKET_NAME)
-AWS_S3_OBJECT_PARAMETERS = {
-    'CacheControl': 'max-age=86400',
-}
-AWS_S3_FILE_OVERWRITE = False
-AWS_DEFAULT_ACL = None
-AWS_LOCATION = 'static'
-
+STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR /'static'
+print("what is base directory: ", BASE_DIR)
 STATICFILES_DIRS = [
     'ecomapp/static',
 ]
-STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
-DEFAULT_FILE_STORAGE = 'ecomapp.media_storages.MediaStorage'
+# AWS_ACCESS_KEY_ID = config('AWS_ACCESS_KEY_ID')
+# AWS_SECRET_ACCESS_KEY = config('AWS_SECRET_ACCESS_KEY')
+# AWS_STORAGE_BUCKET_NAME = config('AWS_STORAGE_BUCKET_NAME')
+
+# AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
+# # print(AWS_STORAGE_BUCKET_NAME)
+# AWS_S3_OBJECT_PARAMETERS = {
+#     'CacheControl': 'max-age=86400',
+# }
+# AWS_S3_FILE_OVERWRITE = False
+# AWS_DEFAULT_ACL = None
+# AWS_LOCATION = 'static'
+
+# STATICFILES_DIRS = [
+#     'ecomapp/static',
+# ]
+# STATIC_URL = 'https://%s/%s/' % (AWS_S3_CUSTOM_DOMAIN, AWS_LOCATION)
+# STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
+
+# DEFAULT_FILE_STORAGE = 'ecomapp.media_storages.MediaStorage'
+
+
 
 # media files configuration
-
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR /'media'
 
